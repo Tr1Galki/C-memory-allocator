2288fac0dd9d1b4a9d498477b94b9859375243e4
Igor Zhirkov <igorjirkov@gmail.com>
2020-12-23 02:14:30 +0100

    Add solution stub

---
+++ Makefile
@@ -0,0 +1,23 @@
+CFLAGS=--std=c17 -Wall -pedantic -Isrc/ -ggdb -Wextra -Werror -DDEBUG
+BUILDDIR=build
+SRCDIR=src
+CC=gcc
+
+all: $(BUILDDIR)/mem.o $(BUILDDIR)/util.o $(BUILDDIR)/mem_debug.o
+	$(CC) -o $(BUILDDIR)/main $^
+
+build:
+	mkdir -p $(BUILDDIR)
+
+$(BUILDDIR)/mem.o: $(SRCDIR)/mem.c build
+	$(CC) -c $(CFLAGS) $< -o $@
+
+$(BUILDDIR)/mem_debug.o: $(SRCDIR)/mem_debug.c build
+	$(CC) -c $(CFLAGS) $< -o $@
+
+$(BUILDDIR)/util.o: $(SRCDIR)/util.c build
+	$(CC) -c $(CFLAGS) $< -o $@
+
+clean:
+	rm -rf $(BUILDDIR)
+
---
+++ src/mem.c
@@ -0,0 +1,130 @@
+#include <stdarg.h>
+#define _DEFAULT_SOURCE
+#include <unistd.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "mem_internals.h"
+#include "mem.h"
+#include "util.h"
+
+void debug_block(struct block_header* b, const char* fmt, ... );
+void debug(const char* fmt, ... );
+
+extern inline block_size size_from_capacity( block_capacity cap );
+extern inline block_capacity capacity_from_size( block_size sz );
+
+static bool            block_is_big_enough( size_t query, struct block_header* block ) { return block->capacity.bytes >= query; }
+static size_t          pages_count   ( size_t mem )                      { return mem / getpagesize() + ((mem % getpagesize()) > 0); }
+static size_t          round_pages   ( size_t mem )                      { return getpagesize() * pages_count( mem ) ; }
+
+static void block_init( void* restrict addr, block_size block_sz, void* restrict next ) {
+  *((struct block_header*)addr) = (struct block_header) {
+    .next = next,
+    .capacity = capacity_from_size(block_sz),
+    .is_free = true
+  };
+}
+
+static size_t region_actual_size( size_t query ) { return size_max( round_pages( query ), REGION_MIN_SIZE ); }
+
+extern inline bool region_is_invalid( const struct region* r );
+
+
+
+static void* map_pages(void const* addr, size_t length, int additional_flags) {
+  return mmap( (void*) addr, length, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | additional_flags , 0, 0 );
+}
+
+/*  аллоцировать регион памяти и инициализировать его блоком */
+static struct region alloc_region  ( void const * addr, size_t query ) {
+  /*  ??? */
+}
+
+static void* block_after( struct block_header const* block )         ;
+
+void* heap_init( size_t initial ) {
+  const struct region region = alloc_region( HEAP_START, initial );
+  if ( region_is_invalid(&region) ) return NULL;
+
+  return region.addr;
+}
+
+#define BLOCK_MIN_CAPACITY 24
+
+/*  --- Разделение блоков (если найденный свободный блок слишком большой )--- */
+
+static bool block_splittable( struct block_header* restrict block, size_t query) {
+  return block-> is_free && query + offsetof( struct block_header, contents ) + BLOCK_MIN_CAPACITY <= block->capacity.bytes;
+}
+
+static bool split_if_too_big( struct block_header* block, size_t query ) {
+  /*  ??? */
+}
+
+
+/*  --- Слияние соседних свободных блоков --- */
+
+static void* block_after( struct block_header const* block )              {
+  return  (void*) (block->contents + block->capacity.bytes);
+}
+static bool blocks_continuous (
+                               struct block_header const* fst,
+                               struct block_header const* snd ) {
+  return (void*)snd == block_after(fst);
+}
+
+static bool mergeable(struct block_header const* restrict fst, struct block_header const* restrict snd) {
+  return fst->is_free && snd->is_free && blocks_continuous( fst, snd ) ;
+}
+
+static bool try_merge_with_next( struct block_header* block ) {
+  /*  ??? */
+}
+
+
+/*  --- ... ecли размера кучи хватает --- */
+
+struct block_search_result {
+  enum {BSR_FOUND_GOOD_BLOCK, BSR_REACHED_END_NOT_FOUND, BSR_CORRUPTED} type;
+  struct block_header* block;
+};
+
+
+static struct block_search_result find_good_or_last  ( struct block_header* restrict block, size_t sz )    {
+  /*??? */
+}
+
+/*  Попробовать выделить память в куче начиная с блока `block` не пытаясь расширить кучу
+ Можно переиспользовать как только кучу расширили. */
+static struct block_search_result try_memalloc_existing ( size_t query, struct block_header* block ) {
+  
+}
+
+
+
+static struct block_header* grow_heap( struct block_header* restrict last, size_t query ) {
+  /*  ??? */
+}
+
+/*  Реализует основную логику malloc и возвращает заголовок выделенного блока */
+static struct block_header* memalloc( size_t query, struct block_header* heap_start) {
+
+  /*  ??? */
+
+}
+
+void* _malloc( size_t query ) {
+  struct block_header* const addr = memalloc( query, (struct block_header*) HEAP_START );
+  if (addr) return addr->contents;
+  else return NULL;
+}
+
+void _free( void* mem ) {
+  if (!mem) return ;
+  struct block_header* header = (struct block_header*)mem-1;
+  header->is_free = true;
+  /*  ??? */
+}
