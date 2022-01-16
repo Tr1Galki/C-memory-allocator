55b816029586c5c13418b181c88aaedafa43370c
Eridan Domoratskiy <eridan200@mail.ru>
2022-01-16 17:17:00 +0300

    Add tests for try_merge_with_next. Upgrade tests for split_if_too_big

--- tester/tests/split_if_too_big.c
+++ tester/tests/split_if_too_big.c
@@ -4,67 +4,70 @@
 
 #include <assert.h>
 
+#define BUFFER_SIZE 512
+#define SMALL_BLOCK_SIZE (BLOCK_MIN_CAPACITY + offsetof(struct block_header, contents) / 2)
+
 // test when block is not free
 DEFINE_TEST(not_free) {
-    uint8_t buffer[128] = { 0 };
+    uint8_t buffer[BUFFER_SIZE] = { 0 };
 
-    block_init(buffer, (block_size) { .bytes = 128 }, NULL);
+    block_init(buffer, (block_size) { .bytes = BUFFER_SIZE }, NULL);
 
     struct block_header * const block = (void*) buffer;
     block->is_free = false;
 
-    assert(!split_if_too_big(block, 120 - BLOCK_MIN_CAPACITY - offsetof(struct block_header, contents)));
+    assert(!split_if_too_big(block, BUFFER_SIZE - SMALL_BLOCK_SIZE - offsetof(struct block_header, contents)));
     assert(block->next == NULL);
-    assert(block->capacity.bytes == 128 - offsetof(struct block_header, contents));
+    assert(block->capacity.bytes == BUFFER_SIZE - offsetof(struct block_header, contents));
     assert(block->is_free == false);
 }
 
 // test when block is not big enough
 DEFINE_TEST(not_big_enough) {
-    uint8_t buffer[128] = { 0 };
+    uint8_t buffer[BUFFER_SIZE] = { 0 };
 
-    block_init(buffer, (block_size) { .bytes = 128 }, NULL);
+    block_init(buffer, (block_size) { .bytes = BUFFER_SIZE }, NULL);
 
     struct block_header * const block = (void*) buffer;
 
-    assert(!split_if_too_big(block, 120));
+    assert(!split_if_too_big(block, BUFFER_SIZE - SMALL_BLOCK_SIZE));
     assert(block->next == NULL);
-    assert(block->capacity.bytes == 128 - offsetof(struct block_header, contents));
+    assert(block->capacity.bytes == BUFFER_SIZE - offsetof(struct block_header, contents));
     assert(block->is_free == true);
 }
 
 // test when block is not big enough for min capacity
 DEFINE_TEST(not_big_enough_for_min_capacity) {
-    uint8_t buffer[128] = { 0 };
+    uint8_t buffer[BUFFER_SIZE] = { 0 };
 
-    block_init(buffer, (block_size) { .bytes = 128 }, NULL);
+    block_init(buffer, (block_size) { .bytes = BUFFER_SIZE }, NULL);
 
     struct block_header * const block = (void*) buffer;
 
-    assert(!split_if_too_big(block, 120 - offsetof(struct block_header, contents)));
+    assert(!split_if_too_big(block, BUFFER_SIZE - SMALL_BLOCK_SIZE - offsetof(struct block_header, contents)));
     assert(block->next == NULL);
-    assert(block->capacity.bytes == 128 - offsetof(struct block_header, contents));
+    assert(block->capacity.bytes == BUFFER_SIZE - offsetof(struct block_header, contents));
     assert(block->is_free == true);
 }
 
 // test successful split
-DEFINE_TEST(successful) {
-    uint8_t buffer[128] = { 0 };
+DEFINE_TEST(success) {
+    uint8_t buffer[BUFFER_SIZE] = { 0 };
 
-    block_init(buffer, (block_size) { .bytes = 128 }, NULL);
+    block_init(buffer, (block_size) { .bytes = BUFFER_SIZE }, NULL);
 
     struct block_header * const block = (void*) buffer;
 
-    assert(split_if_too_big(block, 24));
+    assert(split_if_too_big(block, BLOCK_MIN_CAPACITY));
 
     struct block_header * const next_block = block->next;
 
     assert(next_block != NULL);
-    assert(block->capacity.bytes == 24);
+    assert(block->capacity.bytes == BLOCK_MIN_CAPACITY);
     assert(block->is_free == true);
 
     assert(next_block->next == NULL);
-    assert(next_block->capacity.bytes == 128 - 2 * offsetof(struct block_header, contents) - 24);
+    assert(next_block->capacity.bytes == BUFFER_SIZE - 2 * offsetof(struct block_header, contents) - BLOCK_MIN_CAPACITY);
     assert(next_block->is_free == true);
 }
 
@@ -76,6 +79,6 @@ DEFINE_TEST_GROUP(not_splittable) {
 
 int main() {
     RUN_TEST_GROUP(not_splittable);
-    RUN_SINGLE_TEST(successful);
+    RUN_SINGLE_TEST(success);
     return 0;
 }
