ac333d184c5819df5daf18f72ee56b142ef90023
Alever <aleksg2004@gmail.com>
2022-12-12 00:36:27 +0300

    final?

--- src/tests.c
+++ src/tests.c
@@ -18,14 +18,16 @@ static bool test_allocate_block() {
         return false;
     }
 
-    print_heap(heap, "heap before test_allocate");
+    print_heap(heap, "heap before test_allocate_block");
 
     void *allocated_memory = _malloc(BLOCK_SIZE);
+
+    print_heap(heap, "heap after test_allocate_block");
+
     if (!allocated_memory) {
         printf("test_allocate_block error!\n");
         return false;
     }
-    print_heap(heap, "heap after test_allocate_block");
 
     _free(allocated_memory);
     munmap(heap, size_from_capacity((block_capacity) {.bytes = HEAP_SIZE}).bytes);
@@ -45,20 +47,67 @@ static bool test_allocate_two_blocks() {
     void *first_block = _malloc(BLOCK_SIZE);
     void *second_block = _malloc(BLOCK_SIZE);
 
+    print_heap(heap, "heap after test_allocate_two_blocks");
+
     if (!first_block) {
         if (!second_block) {
-            printf("test_allocate_two_blocks: both bocks corrupted!\n");
+            printf("test_allocate_two_blocks: both blocks corrupted!\n");
         } else {
-            printf("test_allocate_two_blocks: first bock corrupted!\n");
+            printf("test_allocate_two_blocks: first block corrupted!\n");
         }
         return false;
     } else if (!second_block) {
-        printf("test_allocate_two_blocks: second bock corrupted!\n");
+        printf("test_allocate_two_blocks: second block corrupted!\n");
         return false;
     }
 
+    _free(first_block);
+    munmap(heap, size_from_capacity((block_capacity) {.bytes = HEAP_SIZE}).bytes);
+    printf("test_allocate_two_blocks completed");
+    return true;
+}
 
-    print_heap(heap, "heap after test_allocate_two_blocks");
+static bool test_allocate_three_different_blocks() {
+    void *heap = heap_init(HEAP_SIZE);
+    if (!heap) {
+        printf("test_allocate_three_different_blocks error!\n");
+        return false;
+    }
+
+    print_heap(heap, "heap before test_allocate_three_different_blocks");
+
+    void *first_block = _malloc(BLOCK_SIZE/2);
+    void *second_block = _malloc(BLOCK_SIZE*2);
+    void *third_block = _malloc(BLOCK_SIZE);
+
+    print_heap(heap, "heap after test_allocate_three_different_blocks");
+
+    if (!first_block) {
+        if (!second_block) {
+            if (!third_block) {
+                printf("test_allocate_three_different_blocks: all blocks corrupted!\n");
+            } else {
+                printf("test_allocate_three_different_blocks: first and second blocks corrupted!\n");
+            }
+        } else {
+            if (!third_block) {
+                printf("test_allocate_three_different_blocks: first and third blocks corrupted!\n");
+            } else {
+                printf("test_allocate_three_different_blocks: first block corrupted!\n");
+            }
+        }
+        return false;
+    } else if (!second_block) {
+        if (!third_block) {
+            printf("test_allocate_three_different_blocks: second and third blocks corrupted!\n");
+        } else {
+            printf("test_allocate_three_different_blocks: second block corrupted!\n");
+        }
+        return false;
+    } else if (!third_block) {
+        printf("test_allocate_three_different_blocks: third block corrupted!\n");
+        return false;
+    }
 
     _free(first_block);
     munmap(heap, size_from_capacity((block_capacity) {.bytes = HEAP_SIZE}).bytes);
@@ -67,6 +116,6 @@ static bool test_allocate_two_blocks() {
 }
 
 bool tests_run() {
-    bool ret = (test_allocate_block() && test_allocate_two_blocks());
+    bool ret = (test_allocate_block() && test_allocate_two_blocks() && test_allocate_three_different_blocks());
     return ret;
 }
