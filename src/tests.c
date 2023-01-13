bf2db60c49f696ef3b8742e6b660d844da63ecc6
Aleksandr Gasyuk <335066@niuitmo.ru>
2023-01-13 15:12:21 +0000

    Update tests.c

--- src/tests.c
+++ src/tests.c
@@ -7,7 +7,7 @@
 #define BLOCK_SIZE 256
 
 static void print_heap(const void *heap, const char *string) {
-    fprintf( stderr, "%s:\n", string);
+    fprintf(stderr, "%s:\n", string);
     debug_heap(stdout, heap);
 }
 
@@ -19,7 +19,7 @@ static void memory_free(void *heap, void *allocated_memory) {
 static bool test_allocate_block() {
     void *heap = heap_init(HEAP_SIZE);
     if (!heap) {
-        printf("test_allocate_block error!\n");
+        fprintf(stderr, "test_allocate_block error!\n");
         return false;
     }
 
@@ -30,19 +30,19 @@ static bool test_allocate_block() {
     print_heap(heap, "heap after test_allocate_block");
 
     if (!allocated_memory) {
-        printf("test_allocate_block error!\n");
+        fprintf(stderr, "test_allocate_block error!\n");
         return false;
     }
 
     memory_free(heap, allocated_memory);
-    printf("test_allocate_block completed");
+    fprintf(stderr, "test_allocate_block completed");
     return true;
 }
 
 static bool test_allocate_two_blocks() {
     void *heap = heap_init(HEAP_SIZE);
     if (!heap) {
-        printf("test_allocate_two_blocks error!\n");
+        fprintf(stderr, "test_allocate_two_blocks error!\n");
         return false;
     }
 
@@ -55,25 +55,25 @@ static bool test_allocate_two_blocks() {
 
     if (!first_block) {
         if (!second_block) {
-            printf("test_allocate_two_blocks: both blocks corrupted!\n");
+            fprintf(stderr, "test_allocate_two_blocks: both blocks corrupted!\n");
         } else {
-            printf("test_allocate_two_blocks: first block corrupted!\n");
+            fprintf(stderr, "test_allocate_two_blocks: first block corrupted!\n");
         }
         return false;
     } else if (!second_block) {
-        printf("test_allocate_two_blocks: second block corrupted!\n");
+        fprintf(stderr, "test_allocate_two_blocks: second block corrupted!\n");
         return false;
     }
 
     memory_free(heap, first_block);
-    printf("test_allocate_two_blocks completed");
+    fprintf(stderr, "test_allocate_two_blocks completed");
     return true;
 }
 
 static bool test_allocate_three_different_blocks() {
     void *heap = heap_init(HEAP_SIZE);
     if (!heap) {
-        printf("test_allocate_three_different_blocks error!\n");
+        fprintf(stderr, "test_allocate_three_different_blocks error!\n");
         return false;
     }
 
@@ -88,32 +88,32 @@ static bool test_allocate_three_different_blocks() {
     if (!first_block) {
         if (!second_block) {
             if (!third_block) {
-                printf("test_allocate_three_different_blocks: all blocks corrupted!\n");
+                fprintf(stderr, "test_allocate_three_different_blocks: all blocks corrupted!\n");
             } else {
-                printf("test_allocate_three_different_blocks: first and second blocks corrupted!\n");
+                fprintf(stderr, "test_allocate_three_different_blocks: first and second blocks corrupted!\n");
             }
         } else {
             if (!third_block) {
-                printf("test_allocate_three_different_blocks: first and third blocks corrupted!\n");
+                fprintf(stderr, "test_allocate_three_different_blocks: first and third blocks corrupted!\n");
             } else {
-                printf("test_allocate_three_different_blocks: first block corrupted!\n");
+                fprintf(stderr, "test_allocate_three_different_blocks: first block corrupted!\n");
             }
         }
         return false;
     } else if (!second_block) {
         if (!third_block) {
-            printf("test_allocate_three_different_blocks: second and third blocks corrupted!\n");
+            fprintf(stderr, "test_allocate_three_different_blocks: second and third blocks corrupted!\n");
         } else {
-            printf("test_allocate_three_different_blocks: second block corrupted!\n");
+            fprintf(stderr, "test_allocate_three_different_blocks: second block corrupted!\n");
         }
         return false;
     } else if (!third_block) {
-        printf("test_allocate_three_different_blocks: third block corrupted!\n");
+        fprintf(stderr, "test_allocate_three_different_blocks: third block corrupted!\n");
         return false;
     }
     
     memory_free(heap, first_block);
-    printf("test_allocate_two_blocks completed");
+    fprintf(stderr, "test_allocate_two_blocks completed");
     return true;
 }
 
